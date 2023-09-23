b217f6e4ec1c2e96f5d7cdae942b87efadf3cb9e
uwx <maxine@cybergal.com>
2023-09-23 16:23:45 -0300

    Color log output + stacklevel fix

--- build.py
+++ build.py
@@ -156,7 +156,7 @@ def main():
         if args.ci:
             action.start_group(name)
         else:
-            log.info(name)
+            log.info(name, stacklevel=2)
         yield
         if args.ci:
             action.end_group(name)
@@ -165,10 +165,32 @@ def main():
         if args.ci:
             action.error(message, 'ungoogled-chromium build script')
         else:
-            log.error(message)
+            log.error(message, stacklevel=2)
 
     if args.ci:
         log.setLevel(logging.DEBUG)
+        # https://stackoverflow.com/a/56944256
+        class CustomFormatter(logging.Formatter):
+            grey = "\x1b[38;20m"
+            yellow = "\x1b[33;20m"
+            red = "\x1b[31;20m"
+            bold_red = "\x1b[31;1m"
+            reset = "\x1b[0m"
+            format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+
+            FORMATS = {
+                logging.DEBUG: logging.Formatter(grey + format + reset),
+                logging.INFO: logging.Formatter(grey + format + reset),
+                logging.WARNING: logging.Formatter(yellow + format + reset),
+                logging.ERROR: logging.Formatter(red + format + reset),
+                logging.CRITICAL: logging.Formatter(bold_red + format + reset),
+            }
+
+            def format(self, record):
+                formatter = self.FORMATS.get(record.levelno)
+                return formatter.format(record)
+        log.handlers[0].setFormatter(CustomFormatter())
+
 
     # Set common variables
     source_tree = _ROOT_DIR / 'build' / 'src'
