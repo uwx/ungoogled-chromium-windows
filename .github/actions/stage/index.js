49eb471c8a6ae3363d44a8524898fcd7d92f862d
uwx <maxine@cybergal.com>
2023-09-23 01:10:18 -0300

    Refactor stage.js and use zstd for stage compression

--- .github/actions/stage/index.js
+++ .github/actions/stage/index.js
@@ -1,15 +1,33 @@
+// @ts-check
+
 const core = require('@actions/core');
 const io = require('@actions/io');
 const exec = require('@actions/exec');
 const artifact = require('@actions/artifact');
 const glob = require('@actions/glob');
+const path = require('path/win32');
+
+// Where the repository is cloned to
+const basedir = 'C:\\ungoogled-chromium-windows';
+
+const { extractTar, createTar } = require('./tar');
+
+/**
+ * @param {number} ms
+ * @returns {Promise<void>}
+ */
+function delay(ms) {
+    return new Promise(r => setTimeout(r, ms));
+}
 
 async function run() {
     process.on('SIGINT', function() {
-    })
+    });
+
     const finished = core.getBooleanInput('finished', {required: true});
     const from_artifact = core.getBooleanInput('from_artifact', {required: true});
-    const x86 = core.getBooleanInput('x86', {required: false})
+    const x86 = core.getBooleanInput('x86', {required: false});
+
     console.log(`finished: ${finished}, artifact: ${from_artifact}`);
     if (finished) {
         core.setOutput('finished', true);
@@ -20,59 +38,58 @@ async function run() {
     const artifactName = x86 ? 'build-artifact-x86' : 'build-artifact';
 
     if (from_artifact) {
-        await artifactClient.downloadArtifact(artifactName, 'C:\\ungoogled-chromium-windows\\build');
-        await exec.exec('7z', ['x', 'C:\\ungoogled-chromium-windows\\build\\artifacts.zip',
-            '-oC:\\ungoogled-chromium-windows\\build', '-y']);
-        await io.rmRF('C:\\ungoogled-chromium-windows\\build\\artifacts.zip');
+        await artifactClient.downloadArtifact(artifactName, path.join(basedir, 'build'));
+        extractTar(path.join(basedir, 'build', 'artifacts.tar.zstd'), 'zstd', path.join(basedir, 'build'));
+        await io.rmRF(path.join(basedir, 'build', 'artifacts.tar.zstd'));
     }
 
-    const args = ['build.py', '--ci']
-    if (x86)
-        args.push('--x86')
+    const args = ['build.py', '--ci', ...(x86 ? ['--x86'] : [])];
+
     const retCode = await exec.exec('python', args, {
-        cwd: 'C:\\ungoogled-chromium-windows',
+        cwd: basedir,
         ignoreReturnCode: true
     });
     if (retCode === 0) {
         core.setOutput('finished', true);
-        const globber = await glob.create('C:\\ungoogled-chromium-windows\\build\\ungoogled-chromium*',
-            {matchDirectories: false});
-        let packageList = await globber.glob();
-        packageList = await Promise.all(packageList.map(async x => {
-            const part1 = x.substr(0, x.length - 4);
-            const part2 = x86 ? '_x86' : '_x64';
-            const part3 = x.substr(x.length - 4, 4);
-            const newPath = part1 + part2 + part3;
+        const globber = await glob.create(path.join(basedir, 'build', 'ungoogled-chromium*'), {matchDirectories: false});
+
+        /** @type {string[]} */
+        let packageList = [];
+        for await (const x of globber.globGenerator()) {
+            const newPath = x.slice(0, -4) + (x86 ? '_x86' : '_x64') + x.slice(-4);
             await io.mv(x, newPath);
-            return newPath;
-        }));
-        for (let i = 0; i < 5; ++i) {
+            packageList.push(newPath);
+        }
+
+        const maxRetries = 5;
+        for (let i = 0; i < maxRetries; ++i) {
             try {
-                await artifactClient.uploadArtifact(x86 ? 'chromium-x86' : 'chromium', packageList,
-                    'C:\\ungoogled-chromium-windows\\build', {retentionDays: 3});
+                await artifactClient.uploadArtifact(x86 ? 'chromium-x86' : 'chromium', packageList, path.join(basedir, 'build'), {retentionDays: 3});
                 break;
             } catch (e) {
-                console.error(`Upload artifact failed: ${e}`);
+                console.error(`Upload artifact failed: ${e}. Attempt ${i + 1} of ${maxRetries}`);
                 // Wait 10 seconds between the attempts
-                await new Promise(r => setTimeout(r, 10000));
+                await delay(10000);
             }
         }
     } else if (retCode !== 124) {
         core.setOutput('finished', false);
         core.setFailed('Build script returned exit code: ' + retCode);
     } else {
-        await new Promise(r => setTimeout(r, 5000));
-        await exec.exec('7z', ['a', '-tzip', 'C:\\ungoogled-chromium-windows\\artifacts.zip',
-            'C:\\ungoogled-chromium-windows\\build\\src', '-mx=3', '-mtc=on'], {ignoreReturnCode: true});
-        for (let i = 0; i < 5; ++i) {
+        await delay(5000);
+        const globbed = await glob.create(path.join(basedir, 'build', 'src'), { matchDirectories: false }).then(e => e.glob());
+
+        await createTar(path.join(basedir, 'artifacts.tar.zstd'), basedir, globbed, 'zstd');
+
+        const maxRetries = 5;
+        for (let i = 0; i < maxRetries; ++i) {
             try {
-                await artifactClient.uploadArtifact(artifactName, ['C:\\ungoogled-chromium-windows\\artifacts.zip'],
-                    'C:\\ungoogled-chromium-windows', {retentionDays: 3});
+                await artifactClient.uploadArtifact(artifactName, [path.join(basedir, 'artifacts.tar.zstd')], basedir, {retentionDays: 3});
                 break;
             } catch (e) {
-                console.error(`Upload artifact failed: ${e}`);
+                console.error(`Upload artifact failed: ${e}. Attempt ${i + 1} of ${maxRetries}`);
                 // Wait 10 seconds between the attempts
-                await new Promise(r => setTimeout(r, 10000));
+                await delay(10000);
             }
         }
         core.setOutput('finished', false);
---
+++ .github/actions/stage/tar.js
@@ -0,0 +1,123 @@
+// @ts-check
+
+const io = require('@actions/io');
+const fs = require('fs');
+const path = require('path');
+const {exec} = require('@actions/exec');
+
+/*!
+https://github.com/actions/cache/blob/75cd46ec0c4d8108ee45f7507e2bca5517988d31/src/tar.ts
+
+The MIT License (MIT)
+
+Copyright (c) 2018 GitHub, Inc. and contributors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+/**
+ * @param {string[]} args
+ * @returns {Promise<string>}
+ */
+async function getTarPath(args) {
+    return await io.which("tar", true);
+}
+
+/**
+ *
+ * @param {string[]} args
+ * @param {string} [cwd]
+ * @returns {Promise<void>}
+ */
+async function execTar(args, cwd) {
+    try {
+        await exec(`"${await getTarPath(args)}"`, args, { cwd: cwd });
+    } catch (error) {
+        throw new Error(`Tar failed with error: ${error?.message}`);
+    }
+}
+
+/**
+ * @returns {string}
+ */
+function getWorkingDirectory() {
+    return process.env["GITHUB_WORKSPACE"] ?? process.cwd();
+}
+
+/**
+ *
+ * @param {string} archivePath
+ * @param {'zstd' | 'gzip'} compressionMethod
+ * @param {string} [destination]
+ * @returns {Promise<void>}
+ */
+exports.extractTar = async function extractTar(archivePath, compressionMethod, destination) {
+    // Create directory to extract tar into
+    const workingDirectory = destination || getWorkingDirectory();
+    await io.mkdirP(workingDirectory);
+    // --d: Decompress.
+    // --long=#: Enables long distance matching with # bits. Maximum is 30 (1GB) on 32-bit OS and 31 (2GB) on 64-bit.
+    const args = [
+        ...(compressionMethod == 'zstd'
+            ? ["--use-compress-program", "zstd -d --long=31"]
+            : ["-z"]),
+        "-xf",
+        archivePath.replace(new RegExp("\\" + path.sep, "g"), "/"),
+        "-P",
+        "-C",
+        workingDirectory.replace(new RegExp("\\" + path.sep, "g"), "/")
+    ];
+    await execTar(args);
+}
+
+/**
+ * @param {string} tarFileName
+ * @param {string} archiveFolder
+ * @param {string[]} sourceDirectories
+ * @param {'zstd' | 'gzip'} compressionMethod
+ * @returns {Promise<void>}
+ */
+exports.createTar = async function createTar(tarFileName, archiveFolder, sourceDirectories, compressionMethod) {
+    // Write source directories to manifest.txt to avoid command length limits
+    const manifestFilename = "manifest.txt";
+    await fs.promises.writeFile(
+        path.join(archiveFolder, manifestFilename),
+        sourceDirectories.join("\n")
+    );
+    // -T#: Compress using # working thread. If # is 0, attempt to detect and use the number of physical CPU cores.
+    // --long=#: Enables long distance matching with # bits. Maximum is 30 (1GB) on 32-bit OS and 31 (2GB) on 64-bit.
+    // Using 30 here because we also support 32-bit self-hosted runners.
+    const workingDirectory = getWorkingDirectory();
+    const args = [
+        '--posix',
+        ...(compressionMethod == 'zstd'
+            ? ["--use-compress-program", "zstd -T0 --long=31"]
+            : ["-z"]),
+        "-cf",
+        tarFileName.replace(new RegExp("\\" + path.sep, "g"), "/"),
+        '--exclude',
+        tarFileName.replace(new RegExp("\\" + path.sep, "g"), "/"),
+        "-P",
+        "-C",
+        workingDirectory.replace(new RegExp("\\" + path.sep, "g"), "/"),
+        "--files-from",
+        manifestFilename
+    ];
+    await execTar(args, archiveFolder);
+    await fs.promises.unlink(path.join(archiveFolder, manifestFilename));
+}
